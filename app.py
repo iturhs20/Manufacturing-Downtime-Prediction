bf6a487ce6de6303b28603c67fd86d4cc4fda446
iturhs20 <155578005+CreativeTecscapes@users.noreply.github.com>
2025-01-23 17:58:36 +0530

    updates

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify, render_template
-import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
